30bbf5eadc1b05d08f5c269169e9bfe715c5bbb0
Rainvisitor <abc873693@gmail.com>
2025-09-30 01:15:58 +0800

    feat: revert imgur implement

--- packages/ap_common_announcement_ui/lib/src/api/imgbb_helper.dart
+++ packages/ap_common_announcement_ui/lib/src/api/imgbb_helper.dart
@@ -2,13 +2,16 @@ import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:ap_common_flutter_core/ap_common_flutter_core.dart';
-import 'package:path/path.dart' as p;
 
 class ImgurHelper {
   ImgurHelper() {
     dio = Dio(
       BaseOptions(
-        baseUrl: 'https://api.imgur.com',
+        baseUrl: 'https://api.imgbb.com/1',
+        headers: {
+          'Accept': 'application/json',
+          'User-Agent': 'Flutter App',
+        },
       ),
     );
   }
@@ -19,42 +22,60 @@ class ImgurHelper {
 
   late Dio dio;
 
-  static const String clientId = 'bf8e32144d00b04';
+  static const String apiKey = '20778f37dcc08538363199547e461796';
 
   static ImgurHelper? _instance;
 
   Future<ImgurUploadData?> uploadImageToImgur({
     required XFile file,
+    DateTime? expireTime,
     GeneralCallback<ImgurUploadData?>? callback,
   }) async {
     try {
       final Uint8List bytes = await file.readAsBytes();
       final Response<Map<String, dynamic>> response = await dio.post(
-        '/3/image',
-        options: Options(
-          headers: <String, String>{
-            'Authorization': 'Client-ID $clientId',
-          },
-        ),
+        '/upload',
         data: FormData.fromMap(
           <String, dynamic>{
+            'key': apiKey,
             'image': MultipartFile.fromBytes(
               bytes,
-              filename: p.split(file.path).last,
+              filename: file.name,
             ),
+            if (expireTime != null)
+              'expiration': (expireTime.millisecondsSinceEpoch / 1000).round(),
           },
         ),
       );
       if (response.statusCode == 200) {
+        final Map<String, dynamic> imgbbData =
+            response.data!['data'] as Map<String, dynamic>;
+        final Map<String, dynamic> fullResponse = {
+          'status': response.data!['status'],
+          'success': response.data!['success'],
+          'data': <String, dynamic>{
+            'id': imgbbData['id'],
+            'link': imgbbData['url'],
+            'title': imgbbData['title'],
+            'width': imgbbData['width'],
+            'height': imgbbData['height'],
+            'size': imgbbData['size'],
+            'type': imgbbData['mime'],
+            'name': imgbbData['filename'],
+          },
+        };
         final ImgurUploadResponse imgurUploadResponse =
-            ImgurUploadResponse.fromJson(response.data!);
-        return callback == null
-            ? imgurUploadResponse.data
-            : callback.onSuccess(imgurUploadResponse.data) as ImgurUploadData;
+            ImgurUploadResponse.fromJson(fullResponse);
+        if (callback == null) {
+          return imgurUploadResponse.data;
+        } else {
+          callback.onSuccess(imgurUploadResponse.data);
+          return imgurUploadResponse.data;
+        }
       } else {
         callback?.onError(
           GeneralResponse(
-            statusCode: 201,
+            statusCode: 500,
             message:
                 response.statusMessage ?? ApLocalizations.current.unknownError,
           ),
@@ -67,7 +88,7 @@ class ImgurHelper {
           dioException.response?.statusCode == 400) {
         callback?.onError(
           GeneralResponse(
-            statusCode: 201,
+            statusCode: 400,
             message: ApLocalizations.current.notSupportImageType,
           ),
         );
